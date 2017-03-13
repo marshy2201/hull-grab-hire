48ea6e65beeb6abe211715c44702559c15563675
marshy2201 <marshy2201@yahoo.co.uk>
2017-03-13 10:48:15 +0000

    arrows added for slider
    arrows can now be used to move left or right through the slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(() => {
+$(function() {
 
     //toggle nav
-    $('#menu-icon').on('click', () => {
+    $('#menu-icon').on('click', function () {
         $('nav').animate({ width: 'toggle' }, 150);
         $('#menu-icon').toggleClass('show');
 
@@ -10,16 +10,44 @@ $(() => {
             return val == "hidden" ? "scroll" : "hidden";
         });
     });
+     
+    //Arrows change radio input checked
+    $('.arrows').on('click', function () {
+        if ($(this).is('#arrow-right')) {
+            $(this).prev().find('input:checked').next().next().prop('checked', true);
+        }
+
+        if ($(this).is('#arrow-left')) {
+            $(this).prev().prev().find('input:checked').prev().prev().prop('checked', true);
+        }
+    });
 
     //change label colors on click
     $('label').click('click', function () {
         $('label').removeClass('input-checked');
         $(this).addClass('input-checked');
     });
-     
 
-    //document.querySelector('label').style.backgroundColor = '#211F60';
-    //$('label').css({ 'background-color': '#211F60' });
+    //change label colors when radio inputs checked
+    $('.arrows').on('click', function () {
+        if ($('#slide-button-1').is(':checked')) {
+            $('label').removeClass('input-checked');
+            $('#sb1').addClass('input-checked');
+        } else if ($('#slide-button-2').is(':checked')) {
+            $('label').removeClass('input-checked');
+            $('#sb2').addClass('input-checked');
+        } else if ($('#slide-button-3').is(':checked')) {
+            $('label').removeClass('input-checked');
+            $('#sb3').addClass('input-checked');
+        } else if ($('#slide-button-4').is(':checked')) {
+            $('label').removeClass('input-checked');
+            $('#sb4').addClass('input-checked');
+        } else if ($('#slide-button-5').is(':checked')) {
+            $('label').removeClass('input-checked');
+            $('#sb5').addClass('input-checked');
+        }
+    });
+
 
 });
 
